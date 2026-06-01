30b40ccca4f7f92f92178912ec9f84cc37835a94
Thomas Rice <thomas@thomasrice.com>
2026-06-02 00:11:48 +1000

    Export traction load API

--- src/fem3d/__init__.py
+++ src/fem3d/__init__.py
@@ -3,7 +3,7 @@
 from fem3d.boundary import DirichletBC
 from fem3d.material import IsotropicMaterial
 from fem3d.mesh import TetMesh, box_mesh
-from fem3d.solver import LinearElasticityProblem, solve_linear_elasticity
+from fem3d.solver import LinearElasticityProblem, TractionLoad, solve_linear_elasticity
 from fem3d.vtk import write_vtk
 
 __all__ = [
@@ -11,6 +11,7 @@ __all__ = [
     "IsotropicMaterial",
     "LinearElasticityProblem",
     "TetMesh",
+    "TractionLoad",
     "box_mesh",
     "solve_linear_elasticity",
     "write_vtk",
